aefcee9042b52a25dc7acf21af511f9056c24e9d
Arunsam91 <132393567+Arunsam91@users.noreply.github.com>
2023-05-09 00:03:43 +0530

    Update app.py

--- app.py
+++ app.py
@@ -19,4 +19,15 @@ if prompts:
     response = openai.Image.create( prompt=prompts,  n=1,  size="1024x1024")
     image_url = response['data'][0]['url']
     st.image(image_url,use_column_width = True) 
+    
+hide_streamlit_style = """
+            <style>
+            #MainMenu {visibility: hidden;}
+            footer {visibility: hidden;}
+            .css-1y0tads {padding-top: 0rem;}
+
+
+            </style>
+            """
+st.markdown(hide_streamlit_style, unsafe_allow_html=True) 
 
